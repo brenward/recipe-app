f29d244c60d35e60f2c6f6786222d59c79075347
brenward <bren.ward@gmail.com>
2024-03-16 15:13:50 +0000

    #Adding list item display

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Simply a test', 'https://publicdomainrecipes.org/recipes/potato-biscuit-ii/Potato%20Biscuit_hub9ecb69038ff94a4507503835feb7d09_80236_350x350_fill_q75_box_smart1.jpg'),
+    new Recipe('A 2nd Test Recipe', 'Simply a 2nd test', 'https://publicdomainrecipes.org/recipes/potato-biscuit-ii/Potato%20Biscuit_hub9ecb69038ff94a4507503835feb7d09_80236_350x350_fill_q75_box_smart1.jpg')
+  ];
+    
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
