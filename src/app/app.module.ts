719abac5855ba26a4337c0fe7afd37f9724f9c4a
brenward <bren.ward@gmail.com>
2024-03-24 11:48:59 +0000

    #my attempt at opening/closing a dropdown using a custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeItem } from './recipes/recipe-list/recipe-item/recipe-item.compon
 import { RecipeDetail } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { FormsModule } from '@angular/forms';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FormsModule } from '@angular/forms';
     RecipeItem,
     RecipeDetail,
     ShoppingEditComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
