c2fcfab91ade4e9503ac68381d6c56dc1c426939
brenward <bren.ward@gmail.com>
2024-05-22 19:20:06 +0100

    #improving error handling by piping error in service and setting the message

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 interface AuthResponseData{
     kind:string,
@@ -16,6 +18,16 @@ export class AuthService{
     constructor(private http:HttpClient){}
     
     signup(email: string, password:string){
-        return this.http.post<AuthResponseData>(this.url,{email:email, password:password, returnSecureToken: true});
+        return this.http.post<AuthResponseData>(this.url,{email:email, password:password, returnSecureToken: true}).pipe(catchError(errorRes => {
+            let errorMessage = 'An unknown error occurred';
+            if(!errorRes.error || !errorRes.error.error){
+                return throwError(errorMessage);
+            }
+            switch(errorRes.error.error.message){
+                case 'EMAIL_EXISTS':
+                    errorMessage = 'This email already exists';
+            }
+            return throwError(errorMessage);
+        }));
     }
 }
