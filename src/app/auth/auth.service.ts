65df3fc164fe2cd02aeb139535d7120f71ac609b
brenward <bren.ward@gmail.com>
2024-05-25 15:46:07 +0100

    #Storing auth token in local storage and auto logging in if valid token exists

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -39,6 +39,23 @@ export class AuthService{
         this.router.navigate(['/auth']);
     }
 
+    autoLogin(){
+        const userData:{
+            email:string,
+            id: string,
+            _token:string,
+            _tokenExpirationDate:string
+        } = JSON.parse(localStorage.getItem('userData'));
+        if(!userData){
+            return;
+        }
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+        if(loadedUser.token){
+            this.user.next(loadedUser);
+        }
+    }
+
     login(email: string, password:string){
         return this.http.post<AuthResponseData>(this.loginUrl, {email:email, password:password, returnSecureToken: true})
         .pipe(
@@ -77,5 +94,6 @@ export class AuthService{
         const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 }
